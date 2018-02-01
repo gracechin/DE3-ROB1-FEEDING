fa2fd50afb2a54d15765c7ac8f701d86259e04f2
27grace <27grace@gmail.com>
2018-02-01 11:50:37 +0000

    setting up and getting info from robot

--- moveit_python.py
+++ moveit_python.py
@@ -1,1 +1,38 @@
+# Move Group Python Interface
+# http://docs.ros.org/indigo/api/moveit_tutorials/html/doc/pr2_tutorials/planning/scripts/doc/move_group_python_interface_tutorial.html 
 
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+## SETTING UP
+# intialising moveit_commander and rospy
+print "============ Initializing..."
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('move_group_python_interface_tutorial',
+                anonymous=True)
+
+# instantiate objects
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
+group = moveit_commander.MoveGroupCommander("left_arm")
+display_trajectory_publisher = rospy.Publisher(
+                                    '/move_group/display_planned_path',
+                                    moveit_msgs.msg.DisplayTrajectory)
+
+# Wait for RVIZ to initialise (3D visualisation tool)
+print "============ Waiting for RVIZ..."
+rospy.sleep(10)
+print "============ Starting tutorial "
+
+## GETTING BASIC INFORMATION
+print "============ Reference frame: %s" % group.get_planning_frame()
+print "============ Reference frame: %s" % group.get_end_effector_link()
+print "============ Robot Groups:"
+print robot.get_group_names()
+print "============ Printing robot state"
+print robot.get_current_state()
+print "============"
