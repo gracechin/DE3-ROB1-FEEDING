0a688a9ab0f2c46f442be08967c364156f0a9585
27grace <27grace@gmail.com>
2018-02-13 16:02:35 +0000

    using franka and perspective rst updates

---
+++ docs/source/using_franka.rst
@@ -0,0 +1,45 @@
+Using Franka
+============
+
+Setting Up
+^^^^^^^^^^
+
+Visit https://de3-rob1-chess.readthedocs.io to see how franka was setted up.
+
+This 'site <https://frankaemika.github.io>' was used to install the live kernel, libfranka and franka_ros.
+
+Connecting to FRANKA
+^^^^^^^^^^^^^^^^^^^^
+
+Using Wifi
+""""""""""
+
+The FRANKA connection should be automatically done.
+
+
+Using Ethernet Cable
+""""""""""""""""""""
+Create a wireless network using the IP address of the robot and IP address of the computer and use them to connect to the robot. After that you can connect to the web interface.
+
+Using the Web Interface
+"""""""""""""""""""""""
+To connect to the web interface go to https://robot.franka.de.
+
+Without using the lanline to connect FRANKA and the laptop, the web interface can be accessed through the IP address (https://192.168.0.88) to unlock the brakes before starting.
+
+The login is:
+
+    username: robin
+    password: panda
+
+Connection between Franka and the computer terminal can be made done using Wifi or an ethernet cable.
+
+Controlling Franka
+^^^^^^^^^^^^^^^^^^
+
+FRANKA can be controlled using the libfranka library, which is only accessible using the C++ language. Petar and Fabian have made some libfranka functions accessible by Python and the Chess team has made a Python library for FRANKA.
+
+
+Credits 
+^^^^^^^
+Chess team for providing and recording through documentation on basic Franka controls. 
--- human_in_loop.py
+++ human_in_loop.py
@@ -1,9 +1,55 @@
 import numpy as np
+import message_filters
+import subprocess
+import rospy
+from std_msgs.msg import String
+from franka.franka_control import FrankaControl
 
-# ext: get current status
-# ext: get coordinates of mouth
+# initiating caller (https://github.com/nebbles/DE3-ROB1-CHESS/blob/master/franka/caller.py#L9)
+arm = FrankaControl(debug_flag=True) # we set the flag true to get prints to the console about what Caller is doing
+
+# franka status
+
+## MOVING Functions ---------------------------
+# danger function protocol
+def danger():
+	try:
+		# arm moves back by 10cm
+		arm.move_relative(-0.1, 0.0, 0.0)
+	except:
+		print ("Cannot execute danger protocol")
+
+# default protocol function
+def default():
+	# moving to safe position
+	arm.move_absolute([0.0, 0.0, 0.0]) #< still not set
+
+# temp human in loop function
+def human_in_loop():
+	arm.move_relative(0.1, 0.0, 0.0)
 
 
+## SUBSCRIBING MOUTH NODE --------------------------
+# callback function used by the subscriber
+def mouth_node_sub(callback_func):
+	# initialise the node
+	rospy.init_node("chat_subscriber_node", anonymous=True)
+	# subscribe to the chat topic and attach the callback function
+	rospy.Subscriber("/mouthxy", String, callback_func)
+	# loop forever
+	rospy.spin()
 
+def show_message(message):
+    print message.data
 
+## MAIN --------------------------
+def main():
+	try:
+		mouth_node_sub(show_message)
+	except:
+		pass
 
+	for i in range(3):
+		human_in_loop()
+
+main()
