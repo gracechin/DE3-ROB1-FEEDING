fdd8b1b307fe9f6be11f37f4a10893b7199b550b
ROBIN <27grace@gmail.com>
2018-03-05 12:37:37 +0000

    calibration teste

--- fred_feed.py
+++ fred_feed.py
@@ -1,8 +1,7 @@
 import subprocess
 from std_msgs.msg import String
-from franka_custom import FrankaCustom
+from Franka import FrankaCustom
 from franka.franka_control import FrankaControl
-from astra import MouthPos
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -39,7 +38,24 @@ def record_data_pt(uvw_pt, xyz_pt):
 
 ## MAIN --------------------------
 def fred_feed():
-	scale = fred.calibrate()
+	scale = [[0.0015370193568726351, -0.058249999999999871, 9.3565241303452144e-05], [-0.45684233493064297, 10.913749999999977, 0.32972927265227553]]
+	camera_point = fred.get_mouth_pos()
+	go = raw_input("Would you like to go to that camera point? [Y/n]: ")
+	print(go)
+	if (go == '' or go.lower() == 'y'):
+		end = fred.convert_pt(camera_point, scale)
+		end = [str(i) for i in end]
+		start = fred.get_end_effector_pos()
+		print('start:', start)
+		print('end:', end)
+		certain = raw_input("You certain? [Y/n]: ")
+		if (certain == '' or certain.lower() == 'y'):
+			arm.move_absolute(end)
+	fred.get_end_effector_pos()
+	# arm.move_absolute(['0.162', '0.192', '0.658'])
+	# arm.move_relative('0.0', '0.1', '0.0')
+	# fred.get_end_effector_pos()
+	# scale = fred.calibrate()
 	print("Yay!", scale)
 
 fred_feed()
