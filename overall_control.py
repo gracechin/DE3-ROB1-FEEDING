1195dc91ba121ba427c91e300afb90d841c08781
lidiadynesmartinez <lidiadynes@gmail.com>
2018-03-14 15:45:29 +0000

    overall control development, intensive testing required

--- overall_control.py
+++ overall_control.py
@@ -7,39 +7,64 @@ from message_filters import Subscriber
 from baxter_control import BaxterControl
 from reactive import ReactiveControl
 from perception_sub import PerceptionSub
-from Reactive import ReactiveControl
+from reactive import ReactiveControl
 
 
+# Main class for robot control
 class OverallControl:
-	def __init__(self):
-		self.candy_subscription = None
-		self.reactive = ReactiveControl()
+    def __init__(self):
+        self.candy_subscription = None
+        baxter_control = BaxterControl()
+        self.reactive_control = ReactiveControl(baxter_control)
+        self.recorded_control = None # TODO: Recorded class here
 
-	def turn_on_candy(self):
-		self.candy_subscription = rospy.Subscriber("/candy", String, self.__candy_callback__)
+    def begin_execution(self):
+		self.__begin_candy_subscription__()
 
-	def __candy_callback__(self, candy_state):
-		if candy_state:
-			self.go_to_mouth()
-		else: 
-			self.play_recording()
+    def stop_execution(self):
+		self.__unregister_candy_subscription__()
 
-	def play_recording(self):
-		pass
+    def __begin_candy_subscription__(self):
+        if self.candy_subscription is None:
+            self.candy_subscription = rospy.Subscriber("/candy", String, self.__candy_callback__)
 
-    def go_to_mouth(self):
-        self.reactive.turn_on()
+    def __unregister_candy_subscription__(self):
+        if self.candy_subscription is not None:
+            self.candy_subscription.unregister()
+            self.candy_subscription = None
+
+    def __candy_callback__(self, candy_state):
+        if candy_state == 'True':
+            self.__switch_to_reactive__()
+        else:
+            self.__unregister_candy_subscription__()
+            # TODO: Start recording. During this time we should also stop candy subscription.
+            self.__switch_to_recording__()
+            self.__begin_candy_subscription__()
+
+    def __switch_to_reactive__(self):
+        self.reactive_control.turn_on()
+        # TODO: Make sure recorded is off
+
+    def __switch_to_recording__(self):
+        self.reactive_control.turn_off()
+        # TODO: self.recorded_control -> call function for recorded motion
 
 
 def main(args):
-	ps = PerceptionSub()
-	rospy.init_node('overall', anonymous=True)
-	try:
-		rospy.spin()
-	except KeyboardInterrupt:
-		print("Shutting down")
+	oc = OverallControl()
+    ps = PerceptionSub()
+    rc = ReactiveControl(baxter_control)
+
+	oc.begin_execution()
+
+    rospy.init_node('overall', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+
 
 if __name__ == '__main__':
     print("Overall Control")
     main(sys.argv)
-
