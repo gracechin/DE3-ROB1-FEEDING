bec5143ec6401b0f6311e1509ed207a88174b91d
27grace <27grace@gmail.com>
2018-03-24 13:52:09 +0000

    documentation in using_baxter.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,10 @@ sys.path.insert(0, os.path.abspath('../../'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+#    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
